59f4ec15b01e9c1bac925a81ae7770322f9c9dab
Thiagomdr3 <thiago_mdr3@hotmail.com>
2021-10-21 20:27:46 -0300

    Primeira aula dia 21/10

---
+++ Academia.NET/CalculadoraComSwitch/CalculadoraComSwitch/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace SelecaoMes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implemente na calculadora desenvolvida as seguintes funções: Se a operação digitada for “+”, será efetuada a soma; Se a operação digitada for “-”, será efetuada a subtração; Se a operação digitada for “x”, será efetuada a multiplicação Se a operação digitada for “/”, será efetuada a divisão;
+
+            int i = 1;
+
+            while (i == 1)
+            {
+                Console.WriteLine("Digite o valor do primeiro numero:");
+                double num1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o valor do segundo numero:");
+                double num2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o operador (+ , - , / ou x):");
+                char operador = char.Parse(Console.ReadLine());
+
+                switch (operador)
+                {
+                    case '+':
+                        Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+                        Console.WriteLine("Deseja realizar uma nova operação? (0 = não   _   1 = sim)");
+                        i = int.Parse(Console.ReadLine());
+
+                        break;
+
+                    case '-':
+                        Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+                        Console.WriteLine("Deseja realizar uma nova operação? (0 = não   _   1 = sim)");
+                        i = int.Parse(Console.ReadLine());
+                        break;
+
+                    case '/':
+                        Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+                        Console.WriteLine("Deseja realizar uma nova operação? (0 = não   _   1 = sim)");
+                        i = int.Parse(Console.ReadLine());
+                        break;
+
+                    case 'x':
+                        Console.WriteLine("{0} x {1} = {2}", num1, num2, num1 * num2);
+                        Console.WriteLine("Deseja realizar uma nova operação? (0 = não   _   1 = sim)");
+                        i = int.Parse(Console.ReadLine());
+                        break;
+
+                    default:
+                        Console.WriteLine("Você digitou um operador inválido.");
+                        Console.WriteLine("Deseja tentar novamente? (0 = não   _   1 = sim)");
+                        i = int.Parse(Console.ReadLine());
+                        break;
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Academia.NET/ClienteVip/ClienteVip/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ClienteVip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uma loja fornece 10% de desconto para funcionários e 5% de desconto para clientes vips. Faça um programa que calcule o valor total a ser pago por uma pessoa. O programa deverá ler o valor total da compra efetuada e um código que identifique se o comprador é um cliente comum (1), funcionário (2) ou vip (3)
+
+            int i = 1;
+
+            while(i == 1)
+            {
+                Console.WriteLine("Qual o valor da compra?");
+                double compra = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Qual o codigo do cliente? " +
+                    "\n1 = comum;" +
+                    "\n2 = Funcionário;" +
+                    "\n3 = VIP ");
+
+                int codigo = int.Parse(Console.ReadLine());
+
+                switch (codigo)
+                {
+                    case 1:
+                        Console.WriteLine("O valor da sua compra é: " + compra);
+                        Console.WriteLine("Gostaria de registrar uma nova compra? (0 = não    __    1 = sim)");
+                        i = int.Parse(Console.ReadLine());
+                        break;
+
+                    case 2:
+                        compra -= compra * 0.1;
+                        Console.WriteLine("O valor da sua compra é: " + compra);
+                        Console.WriteLine("Gostaria de registrar uma nova compra? (0 = não    __    1 = sim)");
+                        i = int.Parse(Console.ReadLine());
+                        break;
+
+                    case 3:
+                        compra -= compra * 0.05;
+                        Console.WriteLine("O valor da sua compra é: " + compra);
+                        Console.WriteLine("Gostaria de registrar uma nova compra? (0 = não    __    1 = sim)");
+                        i = int.Parse(Console.ReadLine());
+                        break;
+
+                    default:
+                        Console.WriteLine("Tipo de cliente não encontreado");
+                        Console.WriteLine("Gostaria de tentar novamente? (0 = não    __    1 = sim)");
+                        i = int.Parse(Console.ReadLine());
+                        break;
+
+                }
+            }
+            Console.WriteLine("Obrigado. Volte sempre !!");
+
+        }
+    }
+}
