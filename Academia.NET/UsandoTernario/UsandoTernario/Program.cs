fdec09b5986c4af1c521133c2e9af3b7c4f51810
Thiagomdr3 <thiago_mdr3@hotmail.com>
2021-10-21 22:03:36 -0300

    segunda aula dia 21/10

---
+++ Academia.NET/UsandoTernario/UsandoTernario/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UsandoTernario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crie um programa para fazer a leitura de um numero. Se o numero for impar, transforme este numero em par. Se for par, mantenha o mesmo numero
+
+            char x;
+            
+            Console.WriteLine("Digite um numero inteiro");
+            int i = int.Parse(Console.ReadLine());
+
+            x = (i % 2 == 0) ? 'p' : 'i';
+
+            i = (i % 2 == 0) ? i : i + 1;
+            
+            Console.WriteLine("Voce entrou com um numero {0} transformando em par, se necessário ficou: {1}", x,i);
+
+
+        }
+    }
+}
---
+++ Academia.NET/de0a100/de0a100/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Escreva um algoritmo leia pelo teclado um valor inteiro chamado x, e então mostre na tela todos os números de 0 a 100, pulando de x em x.
+            executar();
+
+        }
+
+        public static void executar()
+        {
+            int condicao = 1;
+
+
+
+            while (condicao == 1)
+            {
+
+                Console.WriteLine("Digite um valor de incremento:");
+                int incremento = int.Parse(Console.ReadLine());
+
+                if (incremento >= 100 || incremento < 0)
+                {
+                    Console.WriteLine("Digite um valor positivo e menor que 100");
+                    Console.WriteLine("Gostaria de tentar novamente? (0   =   nao      ____     1    =    sim)");
+                    condicao = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    for (int i = 0; i <= 100; i += incremento)
+                    {
+                        Console.WriteLine("i vale " + i);
+                    }
+                    Console.WriteLine("Gostaria de tentar novamente? (0   =   nao      ____     1    =    sim)");
+                    condicao = int.Parse(Console.ReadLine());
+
+                }
+            }
+        }
+    }
+}
