4effc49864b7eb73c93799fa346b802006f173d4
Thiagomdr3 <thiago_mdr3@hotmail.com>
2021-10-25 23:09:11 -0300

    aula dia 25/10/2021

---
+++ Academia.NET/SequenciaFibonacci/SequenciaFibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SequenciaFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Os dois primeiros números da sequência de Fibonacci são 0 e 1. Qualquer outro número desta sequência pode ser calculado pela soma dos dois imediatamente anteriores: F0=0 ; F1=1 ; Fn=Fn−1+Fn−2
+               Os primeiros números de Fibonacci são: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, …
+               Fazer um algoritmo que leia um número N e mostre os primeiros N números da série de Fibonacci.*/
+
+            Console.WriteLine("Digite um numero para calcularmos a sequencia fibonacci:");
+            int n = int.Parse(Console.ReadLine());
+            int z, y=1, x = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                if(i == 0 || i == 1)
+                {
+                    Console.Write("{0}, ", i);
+                    
+                }
+                else
+                {
+                    z = x + y;
+                    x = y;
+                    y = z;
+                    Console.Write("{0}, ", z);
+                    
+
+
+                }
+            }
+        }
+    }
+}
---
+++ Academia.NET/SplitNumber/SplitNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SplitNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Existem números de 4 dígitos (entre 1000 e 9999) que obedecem à seguinte característica: se dividirmos o número em dois números de dois dígitos, um composto pela dezena e pela unidade, e outro pelo milhar e pela centena, somarmos estes dois novos números gerando um terceiro, o quadrado deste terceiro número é exatamente o número original de quatro dígitos. Por exemplo: 2025 dividindo: 20 e 25 e somando temos 45 e 45² = 2025.
+               Escreva um programa para calcular todos os números que obedecem a esta característica.
+                DICA IMPORTANTE: LEMBREM - SE DA MINHA EXPLICAÇÃO SOBRE RESTO DA DIVISÃO!
+                resto da divisão de um número por 5 = n % 5 - irá variar de 0 até 4.
+                resto da divisão de um número por 4 = n % 4 - irá variar de 0 até 3.
+                resto da divisão de um número por 3 = n % 3 - irá variar de 0 até 2.
+                resto da divisão de um número por 2 = n % 2 - irá variar entre 0 e 1.*/
+
+            int mc, du, soma;
+
+            for (int i = 1000; i <= 9999; i++)
+            {
+                mc = i / 100;
+                du = i % 100;
+
+                soma = mc + du;
+
+                soma *= soma;
+
+                if (soma == i)
+                {
+                    Console.WriteLine(soma);
+                }
+            }
+        }
+    }
+}
